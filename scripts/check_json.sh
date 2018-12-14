d8ec0f912822033f53d52d21673c50e70aa582f3
Robert de Leeuw (IHomer) <robert.de.leeuw@ihomer.nl>
2018-12-14 14:41:20 +0100

    Updated json lint script to output files checked

--- scripts/check_json.sh
+++ scripts/check_json.sh
@@ -6,15 +6,21 @@ echo "================================================="
 echo ""
 
 RETVAL=0
+FILE_COUNT=0
+ERROR_COUNT=0
 for jsonfile in *.json; do
     jsonlint $jsonfile
     if [ $? != 0 ]; then
       echo "ERROR: "
       jsonlint -v $jsonfile
       echo ""
+      ((++ERROR_COUNT));
       RETVAL=1
     fi
+    ((++FILE_COUNT));
 done
 
+echo "JSON files checked: $FILE_COUNT, errors: $ERROR_COUNT";
+echo ""
 exit $RETVAL
 
